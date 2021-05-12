274ee9f90526c4dece49870f5308b99d271d7e12
DoumbiaMamoutou <mamoutoudoumbia89@gmail.com>
2021-05-12 11:57:26 +0000

    update setting

--- couli/settings.py
+++ couli/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&1)t-yls)j$%jhg*-fubeoo@%3-m_(4@(ly5nwci((rz+hx3e0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://couli.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
