bf33a7f25c7e7680807b8fd1fb841246c4033fb3
DoumbiaMamoutou <mamoutoudoumbia89@gmail.com>
2021-05-12 11:48:12 +0000

    update code

--- couli/courseapp/templatetags/my_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter(name='ordering')
-def ordering(content, param='date_add'):
-    data = content.order_by(param)
-    return data
